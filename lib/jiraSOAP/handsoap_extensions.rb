ea1e7e7133dd924c29e21cd02a2afe5bebe26a45
Mark Rada <mrada@marketcircle.com>
2010-10-06 15:42:50 -0400

    Add documentation for the Handsoap extensions

--- lib/jiraSOAP/handsoap_extensions.rb
+++ lib/jiraSOAP/handsoap_extensions.rb
@@ -1,14 +1,24 @@
-#PONDER: send upstream for consideration
+#Some simple extensions to Handsoap.
 module Handsoap
+#Some simple extensions to XmlMason. Currently, this only includes methods to
+#make it easier to build SOAP messages that contain arrays.
 module XmlMason
+  #Represents a node in an XML document
   class Node
-    #TODO: make recursive
+    #@todo Make this method recursive
+    #@param [String] node_name
+    #@param [Array] array
+    #@param [Hash] options
     def add_simple_array(node_name, array = [], options = {})
       prefix, name = parse_ns(node_name)
       node = append_child Element.new(self, prefix, name, nil, options)
       array.each { |element| node.add node_name, element }
     end
 
+    #@todo Make this method recursive
+    #@param [String] node_name
+    #@param [Array] array
+    #@param [Hash] options
     def add_complex_array(node_name, array = [], options = {})
       prefix, name = parse_ns(node_name)
       node = append_child Element.new(self, prefix, name, nil, options)
