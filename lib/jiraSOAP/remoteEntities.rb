a6e832b28536b473102df77464aca45feeceb812
Mark Rada <mrada@marketcircle.com>
2010-10-06 19:07:50 -0400

    Add documentation for all remote objects

--- lib/jiraSOAP/remoteEntities.rb
+++ lib/jiraSOAP/remoteEntities.rb
@@ -3,6 +3,7 @@ module JIRA
 # Represents a priority level. Straightforward.
 class Priority
   attr_accessor :id, :name, :color, :icon, :description
+
   # Factory method that takes a fragment of a SOAP response.
   # @todo change @color to be some kind of hex Fixnum object
   # @todo change @icon to be a URI or NSURL object
@@ -20,9 +21,10 @@ class Priority
   end
 end
 
-# Represents a priority level. Straightforward.
+# Represents a resolution. Straightforward.
 class Resolution
   attr_accessor :id, :name, :icon, :description
+
   # Factory method that takes a fragment of a SOAP response.
   # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
   # @return [JIRA::Resolution,nil]
@@ -37,8 +39,13 @@ class Resolution
   end
 end
 
+# Represents a field mapping.
 class Field
   attr_accessor :id, :name
+
+  # Factory method that takes a fragment of a SOAP response.
+  # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
+  # @return [JIRA::Field,nil]
   def self.field_with_xml_fragment(frag)
     return if frag.nil?
     field      = Field.new
@@ -48,8 +55,14 @@ class Field
   end
 end
 
+# Represents a custom field with values.
+# @todo see if @key is always nil from the server
 class CustomField
   attr_accessor :id, :key, :values
+
+  # Factory method that takes a fragment of a SOAP response.
+  # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
+  # @return [JIRA::CustomField,nil]
   def self.custom_field_with_xml_fragment(frag)
     return if frag.nil?
     custom_field        = CustomField.new
@@ -58,33 +71,51 @@ class CustomField
     custom_field.values = frag.xpath('values/*').map { |value| value.to_s }
     custom_field
   end
+
+  # Generate a SOAP message fragment for the object.
+  # @param [Handsoap::XmlMason::Node] msg SOAP message to add the object to
+  # @param [String] label tag name used in wrapping tags
+  # @return [Handsoap::XmlMason::Element]
   def soapify_for(msg, label = 'customFieldValues')
     msg.add label do |submsg|
       submsg.add 'customfieldId', @id
-      submsg.add 'key', @key #TODO: see if this is always nil
+      submsg.add 'key', @key
       submsg.add_simple_array 'values', @values
     end
   end
 end
 
+# Represents and issue type. Straightforward.
+# @todo change @icon to some kind of URI
 class IssueType
   attr_accessor :id, :name, :icon, :description
   attr_writer   :subtask
+
+  # @return [boolean] true if the issue type is a subtask, otherwise false
   def subtask?; @subtask; end
+
+  # Factory method that takes a fragment of a SOAP response.
+  # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
+  # @return [JIRA::IssueType,nil]
   def self.issue_type_with_xml_fragment(frag)
     return if frag.nil?
     issue_type             = IssueType.new
     issue_type.id          = frag.xpath('id').to_s
     issue_type.name        = frag.xpath('name').to_s
-    issue_type.icon        = frag.xpath('icon').to_s #FIXME: NSURL
+    issue_type.icon        = frag.xpath('icon').to_s
     issue_type.subtask     = frag.xpath('subtask').to_s == 'true'
     issue_type.description = frag.xpath('description').to_s
     issue_type
   end
 end
 
+# Represents a status. Straightforward.
 class Status
   attr_accessor :id, :name, :icon, :description
+
+  # Factory method that takes a fragment of a SOAP response.
+  # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
+  # @return [JIRA::Status,nil]
   def self.status_with_xml_fragment(frag)
     return if frag.nil?
     status             = Status.new
@@ -96,27 +127,44 @@ class Status
   end
 end
 
+# Represents a version for a project. Straightforward.
+# @todo find out why we don't get a description for this object
+# @todo use Time.rfc2822 to make @release_date a Time object
 class Version
   attr_accessor :id, :name, :sequence, :released, :archived, :release_date
   attr_writer   :released, :archived
+
+  # @return [boolean] true if the version has been released, otherwise false
   def released?; @released; end
+
+  # @return [boolean] true if the version has been archive, otherwise false
   def archived?; @archived; end
+
+  # Factory method that takes a fragment of a SOAP response.
+  # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
+  # @return [JIRA::Status,nil]
   def self.version_with_xml_fragment(frag)
     return if frag.nil?
-    #TODO: find out why we don't get a description for this type
     version              = Version.new
     version.id           = frag.xpath('id').to_s
     version.name         = frag.xpath('name').to_s
     version.sequence     = frag.xpath('sequence').to_s.to_i
     version.released     = frag.xpath('released').to_s == 'true'
     version.archived     = frag.xpath('archived').to_s == 'true'
-    version.release_date = frag.xpath('releaseDate') #FIXME: Time.rfc2822
+    version.release_date = frag.xpath('releaseDate')
     version
   end
 end
 
+# Represents a scheme used by the server. Not very useful for the sake of the
+# API; a more useful case might be if you wanted to emulate the server's
+# behaviour.
 class Scheme
   attr_accessor :id, :name, :type, :description
+
+  # Factory method that takes a fragment of a SOAP response.
+  # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
+  # @return [JIRA::Scheme,nil]
   def self.scheme_with_xml_fragment(frag)
     return if frag.nil?
     scheme             = Scheme.new
@@ -128,8 +176,13 @@ class Scheme
   end
 end
 
+# Represents a component description for a project. Straightforward.
 class Component
   attr_accessor :id, :name
+
+  # Factory method that takes a fragment of a SOAP response.
+  # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
+  # @return [JIRA::Component,nil]
   def self.component_with_xml_fragment(frag)
     return if frag.nil?
     component      = Component.new
@@ -139,20 +192,27 @@ class Component
   end
 end
 
+# Represents a project configuration. NOT straightforward.
+# @todo change @url to some kind of URL object
+# @todo change @project_url to some kind of URL object
+# @todo find out why the server always seems to pass nil for schemes
 class Project
   attr_accessor :id, :name, :key, :url, :project_url, :lead, :description
   attr_accessor :issue_security_scheme, :notification_scheme, :permission_scheme
+
+  # Factory method that takes a fragment of a SOAP response.
+  # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
+  # @return [JIRA::Project,nil]
   def self.project_with_xml_fragment(frag)
     return if frag.nil?
     project = Project.new
     project.id                    = frag.xpath('id').to_s
     project.name                  = frag.xpath('name').to_s
     project.key                   = frag.xpath('key').to_s
-    project.url                   = frag.xpath('url').to_s #FIXME: NSURL
-    project.project_url           = frag.xpath('projectUrl').to_s #FIXME: NSURL
+    project.url                   = frag.xpath('url').to_s
+    project.project_url           = frag.xpath('projectUrl').to_s
     project.lead                  = frag.xpath('lead').to_s
     project.description           = frag.xpath('description').to_s
-    #TODO: find out why the server always seems to pass nil
     project.issue_security_scheme =
       Scheme.scheme_with_xml_fragment frag.xpath 'issueSecurityScheme'
     project.notification_scheme   =
@@ -163,10 +223,17 @@ class Project
   end
 end
 
+# Contains a base64 encoded avatar image and some metadata. Straightforward.
 class Avatar
   attr_accessor :id, :owner, :type, :content_type, :base64_data
   attr_writer   :system
+
+  # @return [boolean] true if avatar is the default system avatar, else false
   def system?; @system; end
+
+  # Factory method that takes a fragment of a SOAP response.
+  # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
+  # @return [JIRA::Avatar,nil]
   def self.avatar_with_xml_fragment(frag)
     return if frag.nil?
     avatar              = Avatar.new
@@ -180,14 +247,24 @@ class Avatar
   end
 end
 
-#easily the most convoluted structure in the API
-#will most likely be the greatest source of bugs
+# Represents a JIRA issue; easily the most convoluted structure in the API.
+# This structure and anything related directly to it will most likely be the
+# greatest source of bugs.
+#
+# The irony of the situation is that this structure is also the most critical
+# to have in working order.
+# @todo change @create_date to use Time.rfc2822
+# @todo change @due_date to use Time.rfc2822
 class Issue
   attr_accessor :id, :key, :summary, :description, :type_id, :last_updated
   attr_accessor :votes, :status_id, :assignee_name, :reporter_name, :priority_id
   attr_accessor :project_name, :affects_versions, :create_date, :due_date
   attr_accessor :fix_versions, :resolution_id, :environment, :components
   attr_accessor :attachment_names, :custom_field_values
+
+  # Factory method that takes a fragment of a SOAP response.
+  # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
+  # @return [JIRA::Issue,nil]
   def self.issue_with_xml_fragment(frag)
     return if frag.nil?
     issue                     = Issue.new
@@ -218,21 +295,35 @@ class Issue
     issue.reporter_name       = frag.xpath('reporter').to_s
     issue.priority_id         = frag.xpath('priority').to_s
     issue.project_name        = frag.xpath('project').to_s
-    issue.create_date         = frag.xpath('created').to_s #FIXME: Time.rfc2822
-    issue.due_date            = frag.xpath('duedate').to_s #FIXME: Time.rfc2822
+    issue.create_date         = frag.xpath('created').to_s
+    issue.due_date            = frag.xpath('duedate').to_s
     issue.resolution_id       = frag.xpath('resolution').to_s
     issue.environment         = frag.xpath('environment').to_s
     issue
   end
-  #can you spot the oddities and inconsistencies? (hint: there are many)
-  def soapify_for(msg)
-    #we don't both including fields that are ignored
-      #I tried to only ignore fields that will never be needed at creation
-      #but I may have messed up. It should be easy to fix :)
-    #we don't wrap the whole thing in 'issue' tags for #create_issue calls
-      #this is an inconsistency in the way jiraSOAP works
-      #but you'll probably never know unless you read the source
 
+  # Generate the SOAP message fragment for an issue. Can you spot the oddities
+  # and inconsistencies? (hint: there are many).
+  #
+  # We don't bother including fields that are ignored. I tried to only
+  # ignore fields that will never be needed at creation time, but I may have
+  # messed up.
+  #
+  # We don't wrap the whole thing in 'issue' tags for
+  # {#RemoteAPI#create_issue_with_issue} calls; this is an inconsistency in the
+  # way jiraSOAP works and may need to be worked around for other {RemoteAPI}
+  # methods.
+  #
+  # Servers only seem to accept issues if components/versions are just ids
+  # and do not contain the rest of the {JIRA::Component}/{JIRA::Version}
+  # structure.
+  #
+  # To get the automatic assignee we pass '-1' as the value for @assignee.
+  #
+  # Passing an environment/due date field with a value of nil causes the
+  # server to complain about the formatting of the message.
+  # @param [Handsoap::XmlMason::Node] msg  message the node to add the object to
+  def soapify_for(msg)
     #might be going away, since it appears to have no effect at creation time
     msg.add 'reporter', @reporter_name unless @reporter.nil?
 
@@ -243,7 +334,6 @@ class Issue
     msg.add 'summary', @summary
     msg.add 'description', @description
 
-    #server only accepts issues if components/versions are just ids
     msg.add 'components' do |submsg|
       (@components || []).each { |component|
         submsg.add 'components' do |component_msg|
@@ -265,19 +355,21 @@ class Issue
       }
     end
 
-    #-1 is the value you send to get the automatic assignee
     msg.add 'assignee', (@assignee_name || '-1')
-
     msg.add_complex_array 'customFieldValues', (@custom_field_values || [])
 
-    #passing environment/due_date when nil seems to mess up the server
     msg.add 'environment', @environment unless @environment.nil?
     msg.add 'duedate', @due_date unless @due_date.nil?
   end
 end
 
+# Contains the basic information about a user. Straightforward.
 class User
   attr_accessor :name, :full_name, :email
+
+  # Factory method that takes a fragment of a SOAP response.
+  # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
+  # @return [JIRA::User,nil]
   def self.user_with_xml_fragment(frag)
     return if frag.nil?
     user           = User.new
@@ -288,14 +380,25 @@ class User
   end
 end
 
+# A structure that is a bit of a hack. It is essentially just a key-value pair
+# that is used mainly by {RemoteAPI#update_issue}.
 class FieldValue
   attr_accessor :id, :values
+
+  # Factory method that gives you a nil value for the given id.
+  # @param [String] id name of the field for @values
+  # @return [JIRA::FieldValue] Will always have @values = [nil]
   def self.field_value_with_nil_values(id)
     fv = FieldValue.new
     fv.id = id
     fv.values = [nil]
     fv
   end
+
+  # Generate the SOAP message fragment for a field value.
+  # @param [Handsoap::XmlMason::Node] message the node to add the object to
+  # @param [String] label name for the tags that wrap the message
+  # @return [Handsoap::XmlMason::Element]
   def soapify_for(message, label = 'fieldValue')
     message.add label do |message|
       message.add 'id', @id
