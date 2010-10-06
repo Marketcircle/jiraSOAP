6c04016f66f3211892c773cef7a05abee29aad6d
Mark Rada <mrada@marketcircle.com>
2010-10-06 19:22:17 -0400

    Minor documentation adjustments

--- lib/jiraSOAP/handsoap_extensions.rb
+++ lib/jiraSOAP/handsoap_extensions.rb
@@ -1,24 +1,24 @@
-#Some simple extensions to Handsoap.
+# Some simple extensions to Handsoap.
 module Handsoap
-#Some simple extensions to XmlMason. Currently, this only includes methods to
-#make it easier to build SOAP messages that contain arrays.
+# Some simple extensions to XmlMason. Currently, this only includes methods to
+# make it easier to build SOAP messages that contain arrays.
 module XmlMason
-  #Represents a node in an XML document
+  # Represents a node in an XML document used for SOAP message creation.
   class Node
-    #@todo Make this method recursive
-    #@param [String] node_name
-    #@param [Array] array
-    #@param [Hash] options
+    # @todo Make this method recursive
+    # @param [String] node_name
+    # @param [Array] array
+    # @param [Hash] options
     def add_simple_array(node_name, array = [], options = {})
       prefix, name = parse_ns(node_name)
       node = append_child Element.new(self, prefix, name, nil, options)
       array.each { |element| node.add node_name, element }
     end
 
-    #@todo Make this method recursive
-    #@param [String] node_name
-    #@param [Array] array
-    #@param [Hash] options
+    # @todo Make this method recursive
+    # @param [String] node_name
+    # @param [Array] array
+    # @param [Hash] options
     def add_complex_array(node_name, array = [], options = {})
       prefix, name = parse_ns(node_name)
       node = append_child Element.new(self, prefix, name, nil, options)
--- lib/jiraSOAP/remoteAPI.rb
+++ lib/jiraSOAP/remoteAPI.rb
@@ -3,28 +3,28 @@
 #@todo exception handling
 #@todo code refactoring and de-duplication
 module RemoteAPI
-  #XPath constant to get a node containing a response array.
-  #This could be used for all responses, but is only used in cases where we
-  #cannot use a more blunt XPath expression.
+  # XPath constant to get a node containing a response array.
+  # This could be used for all responses, but is only used in cases where we
+  # cannot use a more blunt XPath expression.
   RESPONSE_XPATH = '/node()[1]/node()[1]/node()[1]/node()[2]'
 
-  #The first method to call; other methods will fail until you are logged in.
-  #@param [String] user JIRA user name to login with
-  #@param [String] password
-  #@return [boolean] true if successful, otherwise false
+  # The first method to call; other methods will fail until you are logged in.
+  # @param [String] user JIRA user name to login with
+  # @param [String] password
+  # @return [boolean] true if successful, otherwise false
   def login(user, password)
     response = invoke('soap:login') { |msg|
       msg.add 'soap:in0', user
       msg.add 'soap:in1', password
     }
-    #cache now that we know it is safe to do so
+    # cache now that we know it is safe to do so
     @user       = user
     @auth_token = response.document.xpath('//loginReturn').first.to_s
     true
   end
 
-  #You only need to call this to make an exlicit logout; normally, a session
-  #will automatically expire after a set time (configured on the server).
+  # You only need to call this to make an exlicit logout; normally, a session
+  # will automatically expire after a set time (configured on the server).
   #@return [boolean] true if successful, otherwise false
   def logout
     response = invoke('soap:logout') { |msg|
@@ -33,8 +33,8 @@ module RemoteAPI
     response.document.xpath('//logoutReturn').first.to_s == 'true'
   end
 
-  #Get the global listing for types of priorities.
-  #@return [[JIRA::Priority]]
+  # Get the global listing for types of priorities.
+  # @return [[JIRA::Priority]]
   def get_priorities
     response = invoke('soap:getPriorities') { |msg|
       msg.add 'soap:in0', @auth_token
@@ -45,8 +45,8 @@ module RemoteAPI
     }
   end
 
-  #Get the global listing for types of resolutions.
-  #@return [[JIRA::Resolution]]
+  # Get the global listing for types of resolutions.
+  # @return [[JIRA::Resolution]]
   def get_resolutions
     response = invoke('soap:getResolutions') { |msg|
       msg.add 'soap:in0', @auth_token
@@ -57,8 +57,8 @@ module RemoteAPI
     }
   end
 
-  #Get the global listing for types of custom fields.
-  #@return [[JIRA::Field]]
+  # Get the global listing for types of custom fields.
+  # @return [[JIRA::Field]]
   def get_custom_fields
     response = invoke('soap:getCustomFields') { |msg|
       msg.add 'soap:in0', @auth_token
@@ -69,8 +69,8 @@ module RemoteAPI
     }
   end
 
-  #Get the global listing for types of issues.
-  #@return [[JIRA::IssueType]]
+  # Get the global listing for types of issues.
+  # @return [[JIRA::IssueType]]
   def get_issue_types
     response = invoke('soap:getIssueTypes') { |msg|
       msg.add 'soap:in0', @auth_token
@@ -81,8 +81,8 @@ module RemoteAPI
     }
   end
 
-  #Get the global listing of status type.
-  #@return [[JIRA::Status]]
+  # Get the global listing of status type.
+  # @return [[JIRA::Status]]
   def get_statuses
     response = invoke('soap:getStatuses') { |msg|
       msg.add 'soap:in0', @auth_token
@@ -93,8 +93,8 @@ module RemoteAPI
     }
   end
 
-  #Get the global listing for notification schemes.
-  #@return [[JIRA::Scheme]]
+  # Get the global listing for notification schemes.
+  # @return [[JIRA::Scheme]]
   def get_notification_schemes
     response = invoke('soap:getNotificationSchemes') { |msg|
       msg.add 'soap:in0', @auth_token
@@ -105,9 +105,9 @@ module RemoteAPI
     }
   end
 
-  #Get all the versions associated with a project.
-  #@param [String] project_key
-  #@return [[JIRA::Version]]
+  # Get all the versions associated with a project.
+  # @param [String] project_key
+  # @return [[JIRA::Version]]
   def get_versions_for_project(project_key)
     response = invoke('soap:getVersions') { |msg|
       msg.add 'soap:in0', @auth_token
@@ -119,9 +119,9 @@ module RemoteAPI
     }
   end
 
-  #Get the information for a project with a given key.
-  #@param [String] project_key
-  #@return [JIRA::Project]
+  # Get the information for a project with a given key.
+  # @param [String] project_key
+  # @return [JIRA::Project]
   def get_project_with_key(project_key)
     response = invoke('soap:getProjectByKey') { |msg|
       msg.add 'soap:in0', @auth_token
@@ -131,9 +131,9 @@ module RemoteAPI
     JIRA::Project.project_with_xml_fragment frag
   end
 
-  #This will only give you basic information about a user.
-  #@param [String] user_name
-  #@return [JIRA::User]
+  # This will only give you basic information about a user.
+  # @param [String] user_name
+  # @return [JIRA::User]
   def get_user_with_name(user_name)
     response = invoke('soap:getUser') { |msg|
       msg.add 'soap:in0', @auth_token
@@ -142,10 +142,10 @@ module RemoteAPI
     JIRA::User.user_with_xml_fragment response.document.xpath '//getUserReturn'
   end
 
-  #Gives you the default avatar image for a project; if you want all
-  #the avatars for a project, use {#get_project_avatars_for_key}.
-  #@param [String] project_key
-  #@return [JIRA::Avatar]
+  # Gets you the default avatar image for a project; if you want all
+  # the avatars for a project, use {#get_project_avatars_for_key}.
+  # @param [String] project_key
+  # @return [JIRA::Avatar]
   def get_project_avatar_for_key(project_key)
     response = invoke('soap:getProjectAvatar') { |msg|
       msg.add 'soap:in0', @auth_token
@@ -154,10 +154,10 @@ module RemoteAPI
     JIRA::Avatar.avatar_with_xml_fragment response.document.xpath '//getProjectAvatarReturn'
   end
 
-  #Gives ALL avatars for a given project use this method; if you
-  #just want the default avatar, use {#get_project_avatar_for_key}.
-  #@param [String] project_key
-  #@return [[JIRA::Avatar]]
+  # Gets ALL avatars for a given project use this method; if you
+  # just want the default avatar, use {#get_project_avatar_for_key}.
+  # @param [String] project_key
+  # @return [[JIRA::Avatar]]
   def get_project_avatars_for_key(project_key)
     response = invoke('soap:getProjectAvatars') { |msg|
       msg.add 'soap:in0', @auth_token
@@ -169,12 +169,12 @@ module RemoteAPI
     }
   end
 
-  #This method is the equivalent of making an advanced search from the
-  #web interface.
-  #@param [String] jql_query JQL query as a string
-  #@param [Fixnum] max_results limit on number of returned results;
+  # This method is the equivalent of making an advanced search from the
+  # web interface.
+  # @param [String] jql_query JQL query as a string
+  # @param [Fixnum] max_results limit on number of returned results;
   #  the value may be overridden by the server if max_results is too large
-  #@return [[JIRA::Issue]]
+  # @return [[JIRA::Issue]]
   def get_issues_from_jql_search(jql_query, max_results = 500)
     response = invoke('soap:getIssuesFromJqlSearch') { |msg|
       msg.add 'soap:in0', @auth_token
@@ -187,29 +187,29 @@ module RemoteAPI
     }
   end
 
-  #This method can update most, but not all, issue fields.
+  # This method can update most, but not all, issue fields.
   #
-  #Fields known to not update via this method:
+  # Fields known to not update via this method:
   #  - status - use {#progress_workflow_action}
   #  - attachments - use {#add_base64_encoded_attachment_to_issue}
   #
-  #Though JIRA::FieldValue objects have an id field, they do not expect
-  #to be given id values. You must use the name of the field you wish to update.
-  #@example Usage With A Normal Field
+  # Though JIRA::FieldValue objects have an id field, they do not expect to be
+  # given id values. You must use the name of the field you wish to update.
+  # @example Usage With A Normal Field
   #  summary        = JIRA::FieldValue.new
   #  summary.id     = 'summary'
   #  summary.values = ['My new summary']
-  #@example Usage With A Custom Field
+  # @example Usage With A Custom Field
   #  custom_field        = JIRA::FieldValue.new
   #  custom_field.id     = 'customfield_10060'
   #  custom_field.values = ['123456']
-  #@example Setting a field to be blank/nil
+  # @example Setting a field to be blank/nil
   #  description = JIRA::FieldValue.field_value_with_nil_values 'description'
-  #@example Calling the method to update an issue
+  # @example Calling the method to update an issue
   #  jira_service_instance.update_issue 'PROJECT-1', description, custom_field
-  #@param [String] issue_key
-  #@param [JIRA::FieldValue] *field_values
-  #@return [JIRA::Issue]
+  # @param [String] issue_key
+  # @param [JIRA::FieldValue] *field_values
+  # @return [JIRA::Issue]
   def update_issue(issue_key, *field_values)
     response = invoke('soap:updateIssue') { |msg|
       msg.add 'soap:in0', @auth_token
@@ -222,7 +222,7 @@ module RemoteAPI
     JIRA::Issue.issue_with_xml_fragment frag
   end
 
-  #Some fields will be ignored when an issue is created:
+  # Some fields will be ignored when an issue is created:
   #  - reporter - you cannot override this value at creation
   #  - due date - I think this is a bug in jiraSOAP or JIRA
   #  - attachments
@@ -230,8 +230,8 @@ module RemoteAPI
   #  - status
   #  - resolution
   #  - environment - I think this is a bug in jiraSOAP or JIRA
-  #@param [JIRA::Issue] issue
-  #@return [JIRA::Issue]
+  # @param [JIRA::Issue] issue
+  # @return [JIRA::Issue]
   def create_issue_with_issue(issue)
     response = invoke('soap:createIssue') { |msg|
       msg.add 'soap:in0', @auth_token
