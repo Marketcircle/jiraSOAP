d6fff447920d56eb050585698e0ed9550e9b7799
Mark Rada <mrada@marketcircle.com>
2010-11-09 23:07:38 -0500

    Add RemoteAPI#add_base64_encoded_attachments_to_issue_with_key

--- lib/jiraSOAP/remoteAPI.rb
+++ lib/jiraSOAP/remoteAPI.rb
@@ -483,6 +483,26 @@ module RemoteAPI
     }
   end
 
+  # @todo optimize building the message, try a single pass
+  # Expect this method to be slow.
+  # @param [String] issue_key
+  # @param [[String]] filenames names to put on the files
+  # @param [[String]] data base64 encoded data
+  # @return [boolean] true if successful, otherwise an exception is thrown
+  def add_base64_encoded_attachments_to_issue_with_key(issue_key, filenames, data)
+    response = invoke('soap:addBase64EncodedAttachmentsToIssue') { |msg|
+      msg.add 'soap:in0', @auth_token
+      msg.add 'soap:in1', issue_key
+      msg.add 'soap:in2' do |submsg|
+        filenames.each { |filename| submsg.add 'filenames', filename }
+      end
+      msg.add 'soap:in3' do |submsg|
+        data.each { |datum| submsg.add 'base64EncodedData', datum }
+      end
+    }
+    true
+  end
+
   # The @build_date attribute is a Time value, but does not include a time.
   # @return [JIRA::ServerInfo]
   def get_server_info
