701b2386ed11a97d40e68442931692c74109d27a
Mark Rada <mrada@marketcircle.com>
2010-11-13 03:35:43 -0500

    Update RemoteAPI regarding the change in Scheme names

--- lib/jiraSOAP/remoteAPI.rb
+++ lib/jiraSOAP/remoteAPI.rb
@@ -91,13 +91,13 @@ module RemoteAPI
     }
   end
 
-  # @return [[JIRA::Scheme]]
+  # @return [[JIRA::NotificationScheme]]
   def get_notification_schemes
     response = invoke('soap:getNotificationSchemes') { |msg|
       msg.add 'soap:in0', @auth_token
     }
     response.document.xpath("#{RESPONSE_XPATH}/getNotificationSchemesReturn").map {
-      |frag| JIRA::Scheme.new frag
+      |frag| JIRA::NotificationScheme.new frag
     }
   end
 
