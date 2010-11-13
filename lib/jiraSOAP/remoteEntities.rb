09acae4f563458cf54103509074ba712de379601
Mark Rada <mrada@marketcircle.com>
2010-11-13 03:34:56 -0500

    Include the IssueProperty section

--- lib/jiraSOAP/remoteEntities.rb
+++ lib/jiraSOAP/remoteEntities.rb
@@ -519,4 +519,54 @@ end
   end
 end
 
+#########################################
+# @pragma mark Inherit from IssueProperty
+#########################################
+
+# Contains all the metadata for a resolution.
+class Resolution < JIRA::IssueProperty
+  # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
+  def initialize(frag = nil)
+    super frag unless frag.nil?
+  end
+end
+
+# Contains all the metadata for an issue's status.
+class Status < JIRA::IssueProperty
+  # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
+  def initialize(frag = nil)
+    super frag unless frag.nil?
+  end
+end
+
+# Contains all the metadata for a priority level.
+# @todo change @color to be some kind of hex Fixnum object
+class Priority < JIRA::IssueProperty
+  # @return [String] is a hex value
+  attr_accessor :color
+
+  # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
+  def initialize(frag = nil)
+    return if frag.nil?
+    super frag
+    @color = frag.xpath('color').to_s
+  end
+end
+
+# Contains all the metadata for an issue type.
+class IssueType < JIRA::IssueProperty
+  # @return [boolean]
+  attr_accessor :subtask
+
+  # @return [boolean] true if the issue type is a subtask, otherwise false
+  def subtask?; @subtask; end
+
+  # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
+  def initialize(frag = nil)
+    return if frag.nil?
+    super frag
+    @subtask = frag.xpath('subTask').to_s == 'true'
+  end
+end
+
 end
