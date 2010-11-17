e96a728deb183da24162fa74cc0a41edf9c45b7e
Mark Rada <mrada@marketcircle.com>
2010-11-16 19:38:07 -0500

    Fix the User class
    It needs to inherit the proper constructor and have a properly named init
    method.

--- lib/jiraSOAP/entities/user.rb
+++ lib/jiraSOAP/entities/user.rb
@@ -2,7 +2,7 @@ module JIRA
 
 # Contains the basic information about a user. The only things missing here
 # are the permissions and login statistics.
-class User
+class User < JIRA::Entity
 
   # @return [String]
   attr_accessor :username
@@ -14,7 +14,7 @@ class User
   attr_accessor :email_address
 
   # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
-  def initialize(frag)
+  def initialize_with_xml_fragment(frag)
     @username, @full_name, @email_address =
       frag.nodes ['name', :to_s], ['fullname', :to_s], ['email', :to_s]
   end
