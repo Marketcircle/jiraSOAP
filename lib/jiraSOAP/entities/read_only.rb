427e184c6169e5742cbe33aa4b05e987437b6f21
Mark Rada <mrada@marketcircle.com>
2010-12-27 02:44:08 -0500

    Update the read-only classes for the new parsing logic
    I also had to make all their instance variables writable.

--- lib/jiraSOAP/entities/read_only.rb
+++ lib/jiraSOAP/entities/read_only.rb
@@ -1,120 +1,116 @@
 module JIRA
 
-# Only contains basic information about the endpoint server and only used
-# by {RemoteAPI#get_server_info}.
-class ServerInfo < JIRA::Entity
-
-  # @return [URL]
-  attr_reader :base_url
-
-  # @return [Time]
-  attr_reader :build_date
-
-  # @return [Fixnum]
-  attr_reader :build_number
-
-  # @return [String]
-  attr_reader :edition
-
-  # @return [JIRA::TimeInfo]
-  attr_reader :server_time
-
-  # @return [String]
-  attr_reader :version
-
-  # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
-  def initialize_with_xml_fragment(frag)
-    @build_date, @build_number, @base_url, @edition, @version, @server_time =
-      frag.nodes( ['buildDate',   :to_date],
-                  ['buildNumber', :to_i],
-                  ['baseUrl',     :to_url],
-                  ['edition',     :to_s],
-                  ['version',     :to_s],
-                  ['serverTime',  :to_object, JIRA::TimeInfo] )
-  end
-end
-
 
 # Simple structure for a time and time zone; only used by JIRA::ServerInfo
 # objects, which themselves are only created when {RemoteAPI#get_server_info}
 # is called.
 class TimeInfo < JIRA::Entity
 
+  @attributes = {
+    'serverTime' => [:server_time=, :to_string_date],
+    'timeZoneId' => [:timezone=,    :to_s]
+  }
+
   # @return [Time]
-  attr_reader :server_time
+  attr_accessor :server_time
 
   # @return [String] in the form of 'America/Toronto'
-  attr_reader :timezone
+  attr_accessor :timezone
 
-  # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
-  def initialize_with_xml_fragment(frag)
-    @server_time, @timezone =
-      frag.nodes ['serverTime', :to_string_date], ['timeZoneId', :to_s]
-  end
+end
+
+
+# Only contains basic information about the endpoint server and only used
+# by {RemoteAPI#get_server_info}.
+class ServerInfo < JIRA::Entity
+
+  @attributes = {
+    'baseUrl'     => [:base_url=,     :to_url],
+    'buildDate'   => [:build_date=,   :to_date],
+    'buildNumber' => [:build_number=, :to_i],
+    'edition'     => [:edition=,      :to_s],
+    'version'     => [:version=,      :to_s],
+    'serverTime'  => [:server_time=,  :to_object, JIRA::TimeInfo]
+  }
+
+  # @return [URL]
+  attr_accessor :base_url
+
+  # @return [Time]
+  attr_accessor :build_date
+
+  # @return [Fixnum]
+  attr_accessor :build_number
+
+  # @return [String]
+  attr_accessor :edition
+
+  # @return [String]
+  attr_accessor :version
+
+  # @return [JIRA::TimeInfo]
+  attr_accessor :server_time
+
 end
 
 
 # A simple structure that is used by {RemoteAPI#get_server_configuration}.
 class ServerConfiguration < JIRA::Entity
 
+  @attributes = {
+    'allowExternalUserManagement' => [:external_user_management_allowed=, :to_boolean],
+    'allowAttachments'            => [:attachments_allowed=,              :to_boolean],
+    'allowIssueLinking'           => [:issue_linking_allowed=,            :to_boolean],
+    'allowSubTasks'               => [:subtasks_allowed=,                 :to_boolean],
+    'allowTimeTracking'           => [:time_tracking_allowed=,            :to_boolean],
+    'allowUnassignedIssues'       => [:unassigned_issues_allowed=,        :to_boolean],
+    'allowVoting'                 => [:voting_allowed=,                   :to_boolean],
+    'allowWatching'               => [:watching_allowed=,                 :to_boolean],
+    'timeTrackingDaysPerWeek'     => [:time_tracking_days_per_week=,      :to_i],
+    'timeTrackingHoursPerDay'     => [:time_tracking_hours_per_day=,      :to_i],
+
+    # in case we are using a version of JIRA that misspells 'management'
+    'allowExternalUserManagment'  => [:external_user_management_allowed=, :to_boolean]
+  }
+
   # @return [boolean]
-  attr_reader :attachments_allowed
+  attr_accessor :attachments_allowed
   alias_method :attachments_allowed?, :attachments_allowed
 
   # @return [boolean]
-  attr_reader :external_user_management_allowed
+  attr_accessor :external_user_management_allowed
   alias_method :external_user_management_allowed?, :external_user_management_allowed
 
   # @return [boolean]
-  attr_reader :issue_linking_allowed
+  attr_accessor :issue_linking_allowed
   alias_method :issue_linking_allowed?, :issue_linking_allowed
 
   # @return [boolean]
-  attr_reader :subtasks_allowed
+  attr_accessor :subtasks_allowed
   alias_method :subtasks_allowed?, :subtasks_allowed
 
   # @return [boolean]
-  attr_reader :time_tracking_allowed
+  attr_accessor :time_tracking_allowed
   alias_method :time_tracking_allowed?, :time_tracking_allowed
 
   # @return [boolean]
-  attr_reader :unassigned_issues_allowed
+  attr_accessor :unassigned_issues_allowed
   alias_method :unassigned_issues_allowed?, :unassigned_issues_allowed
 
   # @return [boolean]
-  attr_reader :voting_allowed
+  attr_accessor :voting_allowed
   alias_method :voting_allowed?, :voting_allowed
 
   # @return [boolean]
-  attr_reader :watching_allowed
+  attr_accessor :watching_allowed
   alias_method :watching_allowed?, :watching_allowed
 
   # @return [Fixnum]
-  attr_reader :time_tracking_days_per_week
+  attr_accessor :time_tracking_days_per_week
 
   # @return [Fixnum]
-  attr_reader :time_tracking_hours_per_day
+  attr_accessor :time_tracking_hours_per_day
 
-  # @param [Handsoap::XmlQueryFront::NokogiriDriver] frag
-  def initialize_with_xml_fragment(frag)
-    @external_user_management_allowed, @attachments_allowed,
-    @issue_linking_allowed,            @subtasks_allowed,
-    @time_tracking_allowed,            @unassigned_issues_allowed,
-    @voting_allowed,                   @watching_allowed,
-    @time_tracking_days_per_week,      @time_tracking_hours_per_day =
-      frag.nodes( ['allowExternalUserManagement', :to_boolean],
-                  ['allowAttachments',            :to_boolean],
-                  ['allowIssueLinking',           :to_boolean],
-                  ['allowSubTasks',               :to_boolean],
-                  ['allowTimeTracking',           :to_boolean],
-                  ['allowUnassignedIssues',       :to_boolean],
-                  ['allowVoting',                 :to_boolean],
-                  ['allowWatching',               :to_boolean],
-                  ['timeTrackingDaysPerWeek',     :to_i],
-                  ['timeTrackingHoursPerDay',     :to_i] )
-    # in case Atlassian still has not fixed their spelling of 'management'
-    @external_user_management_allowed ||= (frag/'allowExternalUserManagment').to_boolean
-  end
 end
 
 end
