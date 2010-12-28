e39991743c12fae3692ae155170b599f64658d59
Mark Rada <mrada@marketcircle.com>
2010-12-28 01:58:57 -0500

    Re-arrange how entities are required
    In preparation of the new parsing logic

--- lib/jiraSOAP/entities.rb
+++ lib/jiraSOAP/entities.rb
@@ -1,26 +1,31 @@
 require 'jiraSOAP/entities/abstract'
+
+module JIRA
+  # Represents a field mapping.
+  class Field < JIRA::NamedEntity
+    add_attributes({})
+  end
+
+  # Represents a component description for a project. It does not include
+  # the component lead.
+  class Component < JIRA::NamedEntity
+    add_attributes({})
+  end
+end
+
 require 'jiraSOAP/entities/read_only'
 require 'jiraSOAP/entities/field_value'
 require 'jiraSOAP/entities/user'
 
 require 'jiraSOAP/entities/avatar'
 require 'jiraSOAP/entities/comment'
-require 'jiraSOAP/entities/issue'
 
 require 'jiraSOAP/entities/filter'
 require 'jiraSOAP/entities/version'
 require 'jiraSOAP/entities/attachments'
-require 'jiraSOAP/entities/project'
+
 require 'jiraSOAP/entities/schemes'
 require 'jiraSOAP/entities/issue_properties'
 
-module JIRA
-  # Represents a field mapping.
-  class Field < JIRA::NamedEntity
-  end
-
-  # Represents a component description for a project. It does not include
-  # the component lead.
-  class Component < JIRA::NamedEntity
-  end
-end
+require 'jiraSOAP/entities/project'
+require 'jiraSOAP/entities/issue'
