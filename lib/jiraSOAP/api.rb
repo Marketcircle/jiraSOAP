e72a834f96ad08d781b1cf4ecec2e29826afceb7
Mark Rada <mrada@marketcircle.com>
2010-12-28 23:04:49 -0500

    Implement RemoteAPI#get_project_roles and friends
    Most friends, but not all of them.

--- lib/jiraSOAP/api.rb
+++ lib/jiraSOAP/api.rb
@@ -6,7 +6,6 @@
 # @todo logging
 # @todo code refactoring and de-duplication
 # @todo break the API down by task, like Apple's developer documentation
-# @todo createProjectRole [v0.6]
 # @todo progressWorkflowAction and friends [target v0.7]
 module RemoteAPI
 
@@ -659,6 +658,76 @@ module RemoteAPI
       msg.add 'soap:in3', base64_image
     }
     true
+  end
+
+  # @todo add tests for this method
+  # @return [[JIRA::ProjectRole]]
+  def get_project_roles
+    response = invoke('soap:getProjectRoles') { |msg|
+      msg.add 'soap:in0', @auth_token
+    }
+    response.document.xpath("#{RESPONSE_XPATH}/getProjectRolesReturn").map {
+      |frag| JIRA::ProjectRole.new_with_xml frag
+    }
+  end
+
+  # @todo add tests for this method
+  # @param [#to_s] role_id
+  # @return [JIRA::ProjectRole]
+  def get_project_role_with_id role_id
+    response = invoke('soap:getProjectRole') { |msg|
+      msg.add 'soap:in0', @auth_token
+      msg.add 'soap:in1', role_id
+    }
+    JIRA::ProjectRole.new_with_xml response.document.xpath '//getProjectRoleReturn'
+  end
+
+  # @todo add tests for this method
+  # @param [JIRA::ProjectRole] project_role
+  # @return [JIRA::ProjectRole] the role that was created
+  def create_project_role_with_role project_role
+    response = invoke('soap:createProjectRole') { |msg|
+      msg.add 'soap:in0', @auth_token
+      msg.add 'soap:in1' do |submsg| project_role.soapify_for submsg end
+    }
+    JIRA::ProjectRole.new_with_xml response.document.xpath '//createProjectRoleReturn'
+  end
+
+  # @todo add tests for this method
+  # @todo true if name does not exist?
+  # @param [String] project_role_name
+  # @return [true,false]
+  def project_role_name_unique? project_role_name
+    response = invoke('soap:isProjectRoleNameUnique') { |msg|
+      msg.add 'soap:in0', @auth_token
+      msg.add 'soap:in1', project_role_name
+    }
+    response.document.xpath('//isProjectRoleNameUniqueReturn').to_boolean
+  end
+
+  # @todo add tests for this method
+  # @todo what is the confirm value for?!?
+  # @param [JIRA::ProjectRole] project_role
+  # @param [true,false] confirm
+  # @return [true]
+  def delete_project_role project_role, confirm = true
+    invoke('soap:deleteProjectRole') { |msg|
+      msg.add 'soap:in0', @auth_token
+      msg.add 'soap:in1' do |submsg| project_role.soapify_for submsg end
+      msg.add 'soap:in2', confirm
+    }
+    true
+  end
+
+  # @todo add tests for this method
+  # @param [JIRA::ProjectRole] project_role
+  # @return [JIRA::ProjectRole] the role after the update
+  def update_project_role_with_role project_role
+    response = invoke('soap:updateProjectRole') { |msg|
+      msg.add 'soap:in0', @auth_token
+      msg.add 'soap:in1' do |submsg| project_role.soapify_for submsg end
+    }
+    JIRA::ProjectRole.new_with_xml response.document.xpath '//updateProjectRoleReturn'
   end
 
 end
