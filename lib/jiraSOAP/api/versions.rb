2233ad46f97145c92bba7998e951554a6eb3074a
Mark Rada <mrada@marketcircle.com>
2011-01-10 00:29:04 -0500

    Move RemoteAPI methods related to versions into their own file
    And also into their own group (this is reflected in the documentation).

---
+++ lib/jiraSOAP/api/versions.rb
@@ -0,0 +1,70 @@
+module JIRA
+module RemoteAPI
+  # @group Working with Versions
+
+  # @param [String] project_key
+  # @return [[JIRA::Version]]
+  def get_versions_for_project project_key
+    response = invoke('soap:getVersions') { |msg|
+      msg.add 'soap:in0', @auth_token
+      msg.add 'soap:in1', project_key
+    }
+    response.document.xpath("#{RESPONSE_XPATH}/getVersionsReturn").map {
+      |frag| JIRA::Version.new_with_xml frag
+    }
+  end
+
+  # New versions cannot have the archived bit set and the release date
+  # field will ignore the time of day you give it and instead insert
+  # the time zone offset as the time of day.
+  #
+  # Remember that the @release_date field is the tentative release date,
+  # so its value is independant of the @released flag.
+  #
+  # Descriptions do not appear to be included with JIRA::Version objects
+  # that SOAP API provides.
+  # @param [String] project_key
+  # @param [JIRA::Version] version
+  # @return [JIRA::Version]
+  def add_version_to_project_with_key project_key, version
+    response = invoke('soap:addVersion') { |msg|
+      msg.add 'soap:in0', @auth_token
+      msg.add 'soap:in1', project_key
+      msg.add 'soap:in2' do |submsg| version.soapify_for submsg end
+    }
+    JIRA::Version.new_with_xml response.document.xpath('//addVersionReturn').first
+  end
+
+  # The archive state can only be set to true for versions that have not been
+  # released. However, this is not reflected by the return value of this method.
+  # @param [String] project_key
+  # @param [String] version_name
+  # @param [true,false] state
+  # @return [true]
+  def set_archive_state_for_version_for_project project_key, version_name, state
+    invoke('soap:archiveVersion') { |msg|
+      msg.add 'soap:in0', @auth_token
+      msg.add 'soap:in1', project_key
+      msg.add 'soap:in2', version_name
+      msg.add 'soap:in3', state
+    }
+    true
+  end
+
+  # You can set the release state for a project with this method.
+  # @param [String] project_name
+  # @param [JIRA::Version] version
+  # @return [true]
+  def release_state_for_version_for_project project_name, version
+    invoke('soap:releaseVersion') { |msg|
+      msg.add 'soap:in0', @auth_token
+      msg.add 'soap:in1', project_name
+      msg.add 'soap:in2' do |submsg| version.soapify_for submsg end
+    }
+    true
+  end
+
+  # @endgroup
+end
+end
+
