089d9e5f625ba158394b3d4743858398074ec210
Mark Rada <mrada@marketcircle.com>
2011-02-07 03:07:45 -0500

    Put RemoteAPI#get_permission_schemes back
    When did I lose it?

--- lib/jiraSOAP/api/schemes.rb
+++ lib/jiraSOAP/api/schemes.rb
@@ -7,6 +7,11 @@ module RemoteAPI
     jira_call JIRA::NotificationScheme, 'getNotificationSchemes'
   end
 
+  # @return [Array<JIRA::PermissionScheme>]
+  def get_permission_schemes
+    jira_call JIRA::PermissionScheme, 'getPermissionSchemes'
+  end
+
   # @endgroup
 end
 end
