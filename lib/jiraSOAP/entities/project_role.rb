3e13d48d610cc1d36e87ff011989f4ad78ea72d8
Mark Rada <mrada@marketcircle.com>
2011-02-21 01:05:41 -0500

    Get rid of #soapify_for for ProjectRole objects
    #to_soap works out of the box in this case.

--- lib/jiraSOAP/entities/project_role.rb
+++ lib/jiraSOAP/entities/project_role.rb
@@ -1,11 +1,4 @@
 # Only a name, description and id.
 class JIRA::ProjectRole < JIRA::DescribedEntity
   add_attributes
-
-  # @param [Handsoap::XmlMason::Node] msg the node where to add self
-  def soapify_for msg
-    msg.add 'id', @id
-    msg.add 'name', @name
-    msg.add 'description', @description
-  end
 end
