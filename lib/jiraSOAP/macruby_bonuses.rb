55d8f0ac958edb40bc0e7562e73509923f456bc0
Mark Rada <mrada@marketcircle.com>
2011-04-07 02:10:50 -0400

    Replace NSURL#to_s implementation with a method alias

--- lib/jiraSOAP/macruby_bonuses.rb
+++ lib/jiraSOAP/macruby_bonuses.rb
@@ -3,14 +3,7 @@ framework 'Foundation'
 # In the case of MacRuby, we extend NSURL to behave enough like a
 # URI::HTTP object that they can be interchanged.
 class NSURL
-
-  # We have to override, using {#alias_method} does not work because
-  # {#to_s} is defined in the base class.
-  # @return [String]
-  def to_s
-    absoluteString
-  end
-
+  alias_method :to_s, :absoluteString
 end
 
 
