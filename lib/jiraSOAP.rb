28fb7bf9fe2f6bf37bc6d557df1eee76d61fd96c
Mark Rada <mrada@marketcircle.com>
2011-05-28 21:40:19 -0400

    Cleanup top level file

--- lib/jiraSOAP.rb
+++ lib/jiraSOAP.rb
@@ -1,14 +1,17 @@
 require 'time'
 require 'uri'
+
 require 'nokogiri'
+require 'jiraSOAP/nokogiri_extensions'
 
 require 'handsoap'
 Handsoap.http_driver = :net_http
+require 'jiraSOAP/handsoap_extensions'
 
 require 'jiraSOAP/url'
-require 'jiraSOAP/handsoap_extensions'
-require 'jiraSOAP/nokogiri_extensions'
+require 'jiraSOAP/macruby_extensions' if RUBY_ENGINE == 'macruby'
 
+##
 # All the remote entities as well as the SOAP service client.
 module JIRA
 end
@@ -17,5 +20,3 @@ require 'jiraSOAP/version'
 require 'jiraSOAP/entities'
 require 'jiraSOAP/api'
 require 'jiraSOAP/JIRAservice'
-
-require 'jiraSOAP/macruby_bonuses' if RUBY_ENGINE == 'macruby'
