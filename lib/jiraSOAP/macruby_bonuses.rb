45315cfdfa3fc3decd99d7139a3649741af53625
Mark Rada <mrada@marketcircle.com>
2011-05-28 21:26:26 -0400

    Cleanup MacRuby bonuses

--- lib/jiraSOAP/macruby_bonuses.rb
+++ lib/jiraSOAP/macruby_bonuses.rb
@@ -1,14 +1,13 @@
+##
 # In the case of MacRuby, we extend NSURL to behave enough like a
 # URI::HTTP object that they can be interchanged.
 class NSURL
   alias_method :to_s, :absoluteString
 end
 
-
-# @todo get a parallel map method for collections
 module JIRA
-
   @url_class       = NSURL
   @url_init_method = :'URLWithString:'
-
 end
+
+# @todo get a parallel map method for collections
