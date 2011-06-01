a70c6f05965273fe6a76d036be883f48a4879435
Mark Rada <mrada@marketcircle.com>
2011-06-01 01:51:29 -0400

    Switch AttachmentMetadata over to the new style
    Which also allows the use of the new YARD property detection.
    
    Also set up some method aliases.

--- lib/jiraSOAP/entities/attachment_metadata.rb
+++ lib/jiraSOAP/entities/attachment_metadata.rb
@@ -3,14 +3,23 @@
 # appears to be of the form
 # "{JIRA::JIRAService.endpoint_url}/secure/attachment/{#id}/{#file_name}"
 class JIRA::AttachmentMetadata < JIRA::NamedEntity
-  add_attributes(
-    ['author',   :author,      :content],
-    ['filename', :file_name,   :content],
-    ['mimetype', :mime_type,   :content],
-    # Measured in bytes
-    ['filesize', :file_size,   :to_i],
-    ['created',  :create_time, :to_iso_date]
-  )
+
+  # @return [String]
+  add_attribute :author, 'author', :content
+
+  # @return [String]
+  add_attribute :file_name, 'filename', :content
+  alias_method :filename, :file_name
+
+  # @return [String]
+  add_attribute :mime_type, 'mimetype', :content
+  alias_method :content_type, :mime_type
+
+  # @return [Number] Measured in bytes
+  add_attribute :file_size, 'filesize', :to_i
+
+  # @return [Time]
+  add_attribute :create_time, 'created', :to_iso_date
 
   ##
   # Fetch the attachment from the server.
