722427d57c213e4193bc9101572ebff1e2e15554
Mark Rada <mrada@marketcircle.com>
2011-06-10 16:29:48 -0400

    Backport macruby_extensions from the 0.8-dev branch

--- lib/jiraSOAP.rb
+++ lib/jiraSOAP.rb
@@ -8,6 +8,7 @@ Handsoap.http_driver = :net_http
 require 'jiraSOAP/url'
 require 'jiraSOAP/handsoap_extensions'
 require 'jiraSOAP/nokogiri_extensions'
+require 'jiraSOAP/macruby_extensions' if RUBY_ENGINE == 'macruby'
 
 # All the remote entities as well as the SOAP service client.
 module JIRA
@@ -17,4 +18,3 @@ require 'jiraSOAP/entities'
 require 'jiraSOAP/api'
 require 'jiraSOAP/JIRAservice'
 
-require 'jiraSOAP/macruby_bonuses' if RUBY_ENGINE == 'macruby'
---
+++ lib/jiraSOAP/macruby_extensions.rb
@@ -0,0 +1,14 @@
+##
+# In the case of MacRuby, we extend NSURL to behave enough like a
+# URI::HTTP object that they can be interchanged.
+class NSURL
+  alias_method :to_s, :absoluteString
+  alias_method :inspect, :absoluteString
+end
+
+module JIRA
+  @url_class       = NSURL
+  @url_init_method = :'URLWithString:'
+end
+
+# @todo get a parallel map method for collections
