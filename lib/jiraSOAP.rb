7f10d4d2f2395df0e7344f26ecc5960c2a123679
Mark Rada <mrada@marketcircle.com>
2011-06-10 16:31:37 -0400

    Backport updates to the nokogiri monkey patches

--- lib/jiraSOAP.rb
+++ lib/jiraSOAP.rb
@@ -1,13 +1,14 @@
 require 'time'
 require 'uri'
+
 require 'nokogiri'
+require 'jiraSOAP/nokogiri_extensions'
 
 require 'handsoap'
 Handsoap.http_driver = :net_http
 
 require 'jiraSOAP/url'
 require 'jiraSOAP/handsoap_extensions'
-require 'jiraSOAP/nokogiri_extensions'
 require 'jiraSOAP/macruby_extensions' if RUBY_ENGINE == 'macruby'
 
 # All the remote entities as well as the SOAP service client.
