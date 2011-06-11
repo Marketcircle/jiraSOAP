81d683127a54fcf848c6bb3acd8cee8992bbbb5d
Mark Rada <mrada@marketcircle.com>
2011-06-10 23:26:57 -0400

    Backport style changes to the Handsoap extensions

--- lib/jiraSOAP/handsoap_extensions.rb
+++ lib/jiraSOAP/handsoap_extensions.rb
@@ -1,46 +1,37 @@
-# Some simple extensions to Handsoap.
-module Handsoap
-
-# @todo check if these methods already exist in Handsoap::Service
-# Some extensions to the XML builder to make message building less ugly.
-module XmlMason
-
-  # A node in a Nokogiri XML document.
-  class Node
-
-    # @todo Make this method recursive
-    # @param [String] node_name
-    # @param [Array] array
-    # @param [Hash] options
-    def add_simple_array(node_name, array = [], options = {})
-      prefix, name = parse_ns(node_name)
-      node = append_child Element.new(self, prefix, name, nil, options)
-      array.each { |element| node.add node_name, element }
-    end
-
-    # @todo Make this method recursive
-    # @param [String] node_name
-    # @param [Array] array
-    # @param [Hash] options
-    def add_complex_array(node_name, array = [], options = {})
-      prefix, name = parse_ns(node_name)
-      node = append_child Element.new(self, prefix, name, nil, options)
-      array.each { |element| element.soapify_for node, name }
-    end
-
+##
+# @todo Push these upstream?
+#
+# Some simple extensions to Handsoap to make building SOAP messages easier.
+class Handsoap::XmlMason::Node
+  ##
+  # @todo Make this method recursive
+  #
+  # @param [String] node_name
+  # @param [Array] array
+  # @param [Hash] options
+  def add_simple_array node_name, array = [], options = {}
+    prefix, name = parse_ns(node_name)
+    node = append_child Element.new(self, prefix, name, nil, options)
+    array.each { |element| node.add node_name, element }
   end
 
-end
-
-
-# Exposing an attribute in the underlying NokogiriDriver class.
-module XmlQueryFront
-
-  # Monkey patch to expose the underlying Nokogiri object
-  class NokogiriDriver
-    # @return [Nokogiri::XML::Element]
-    attr_reader :element
+  ##
+  # @todo Make this method recursive
+  #
+  # @param [String] node_name
+  # @param [Array] array
+  # @param [Hash] options
+  def add_complex_array node_name, array = [], options = {}
+    prefix, name = parse_ns(node_name)
+    node = append_child Element.new(self, prefix, name, nil, options)
+    array.each { |element| element.soapify_for node, name }
   end
-
 end
+
+##
+# Monkey patch to expose the underlying Nokogiri object as jiraSOAP
+# can parse much faster without the Handsoap layer in between.
+class Handsoap::XmlQueryFront::NokogiriDriver
+  # @return [Nokogiri::XML::Element]
+  attr_reader :element
 end
