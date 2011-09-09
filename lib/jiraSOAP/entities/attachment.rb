b9cac94fd45dbc42ebea732997d67ce993dc07ee
Randy Harmon <rharmon@rocketfuel.com>
2011-09-08 17:20:48 -0700

    Use Attachment objects for uploading issues
    This provides a more rubyish interface for file uploads.
    
    The prior add_base64_encoded_attachments_to_issue_with_key method is retained but deprecated

--- lib/jiraSOAP/entities/attachment.rb
+++ lib/jiraSOAP/entities/attachment.rb
@@ -2,7 +2,7 @@
 # Only contains the metadata for an attachment. The URI for an attachment
 # appears to be of the form
 # "{JIRA::JIRAService.endpoint_url}/secure/attachment/{#id}/{#file_name}"
-class JIRA::AttachmentMetadata < JIRA::NamedEntity
+class JIRA::Attachment < JIRA::NamedEntity
 
   # @return [String]
   add_attribute :author, 'author', :content
@@ -12,6 +12,21 @@ class JIRA::AttachmentMetadata < JIRA::NamedEntity
   alias_method :filename, :file_name
   alias_method :filename=, :file_name=
 
+  # @return [String]
+  add_attribute :file_name, 'filename', :content
+
+  ##
+  # Content to be used for adding attachments, using #add_attachments_to_issue_with_key
+  #
+  # Note that this method does not allow you to read the content of an existing attachment on the issue; only the
+  # metadata for the attachment may be read at this time.
+  #
+  # @return [String]
+  add_attribute :content, nil, :content
+  def content
+    raise NotImplementedError, "Content reading not currently supported.  Patches welcome."
+  end
+  
   # @return [String]
   add_attribute :mime_type, 'mimetype', :content
   alias_method :content_type, :mime_type
@@ -31,5 +46,4 @@ class JIRA::AttachmentMetadata < JIRA::NamedEntity
   def attachment
     raise NotImplementedError, 'Please implement me. :('
   end
-
 end
